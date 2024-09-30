c80b9a91b9f41799935b72d61d75bd538b032487
Christian brito <114140097+ARMILANCER@users.noreply.github.com>
2024-09-30 09:31:45 +0200

    Update Raf.java

--- java/work/Raf.java
+++ java/work/Raf.java
@@ -1,13 +1,21 @@
 package work;
-import javafx.beans.binding.IntegerBinding;
+
+
+
+import com.sun.jdi.Value;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
 import java.io.IOException;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Raf {
-    private StringBuilder strBdr;
     private String pathF;
-    public Raf(String pathF,int length){
+    private Vector<Integer> data = new Vector<>();
+    public Raf(String pathF){
         this.pathF = pathF;
     }
 
@@ -15,11 +23,29 @@ public class Raf {
         try (RandomAccessFile raf = new RandomAccessFile(pathF, "rw")) {
             switch (choice){
                 case 1:
-                    System.out.printf("In: ");
-                    write(raf,new Scanner(System.in).nextInt());
+                    System.out.print("In: ");
+                    Scanner scanner = new Scanner(System.in);
+                    String input;
+                    while (true) {
+                        System.out.print("Inserisci un numero intero (o 'exit' per uscire): ");
+                        input = scanner.nextLine();
+                        if (input.equalsIgnoreCase("exit")) {
+                            break;
+                        }
+                        try {
+                            data.add(Integer.parseInt(input));
+                        } catch (NumberFormatException e) {
+                            System.out.println("no");
+                        }
+                    }
+                    write(raf,data);
                     break;
                 case 2:
-                    read(raf);
+                    Vector<Integer> data = read(raf);
+                    for (int i = 0; i < data.size(); i++) System.out.println("ID: "+i+" Num: "+data.get(i));
+                    break;
+                case 3:
+                    delete(raf,new Scanner(System.in).nextInt());
                     break;
                 default:
                     System.out.println("Really..?");
@@ -29,30 +55,31 @@ public class Raf {
         }
     }
 
-    private void write(RandomAccessFile raf,Integer num) throws IOException{
+    private void write(RandomAccessFile raf,Vector<Integer>data) throws IOException{
         raf.seek(raf.length());
-        raf.writeInt(num);
+        for (Integer num : data) raf.writeInt(num);
     }
 
-    private void read(RandomAccessFile raf) throws IOException{
-        System.out.print("Numbers: ");
-        while(raf.getFilePointer()<raf.length()){
-            System.out.print(raf.readInt()+", ");
-        }
-        System.out.println();
+    private Vector<Integer> read(RandomAccessFile raf) throws IOException{
+        data = new Vector<>();
+        while(raf.getFilePointer()<raf.length()) data.add(raf.readInt());
+        return data;
     }
 
     private void delete(RandomAccessFile raf, int posizione) throws IOException{
+        data = read(raf);
+        data.remove(posizione);
+        raf.setLength(0);
+        write(raf,data);
     }
 
     public static void main(String[] args){
         // pas the absolute path why the relative is not found.
-        Raf raf = new Raf("/Users/christianbrito/Documents/Eli/eli/src/main/java/work/eli.csv",10);
+        Raf raf = new Raf("/Users/christianbrito/Documents/Eli/eli/src/main/java/work/eli.csv");
         boolean stop = false;
         while (!stop){
             System.out.print("Choice: ");
             raf.gestionFile(new Scanner(System.in).nextInt());
         }
-
     }
 }
