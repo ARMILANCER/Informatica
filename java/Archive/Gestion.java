1641694118c4639c3a4fa5664a9ecece4e8ba39a
Christian brito <114140097+ARMILANCER@users.noreply.github.com>
2024-10-11 10:25:41 +0200

    Update Gestion.java

--- java/Archive/Gestion.java
+++ java/Archive/Gestion.java
@@ -1,29 +1,34 @@
 package Archive;
-
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
+import java.util.*;
 public class Gestion {
     private StringBuilder tupla = new StringBuilder();
     private Album album;
-    private Map map;
-    private Scanner scanner=new Scanner(System.in);
+    private LinkedList<Map> indexing = new LinkedList<>();
+    private String path;
+    private int counter = 0;
+
+    public Gestion(String path) {
+        this.path = path;
+        try(RandomAccessFile raf = new RandomAccessFile(path, "rw")){
+            formatCounter(raf);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void formatCounter(RandomAccessFile raf) throws IOException {
+        raf.seek(raf.length()-128);
+    }
+
     public void gestionFile(int choice) {
-        try (RandomAccessFile raf = new RandomAccessFile("../example.csv", "rw")) {
+        try (RandomAccessFile raf = new RandomAccessFile(path, "rw")) {
             switch (choice) {
                 case 1:
-                    boolean stop = false;
-                    while(!scanner.nextLine().equals("exit")){
-                        album = new Album("album","artist",0000);
-                        indexingFile(raf,album);
-                    }
+                    album = new Album("album della signa","Umanita ",2);
                     write(raf, album);
-
                     break;
                 case 2:
                     read(raf);
@@ -34,8 +39,8 @@ public class Gestion {
         }
     }
 
-    public void indexingFile(RandomAccessFile raf, Album album) throws IOException {
-        map.addElement();
+    public void indexingFile(Album album){
+        indexing.add(new Map(album.getName(),counter));
     }
 
     private void filling() {
@@ -47,38 +52,34 @@ public class Gestion {
 
     private void write(RandomAccessFile raf,Album album) throws IOException {
         filling();
-        tupla.replace(0, text.length(), text);
-        tupla.replace(60, 60 + text.length(), text);
+        tupla.replace(0, album.getName().length(), album.getName());
+        tupla.replace(60, 60 + album.getArtist().length(), album.getArtist());
         //tupla.replace(120,120+4,"4234");
         raf.seek(raf.length());
+        raf.writeInt(counter);
         raf.writeBytes(tupla.toString());
-        raf.writeInt(9910);
+        raf.writeInt(album.getYear());
         System.out.println(raf.length());
     }
 
     private void read(RandomAccessFile raf) throws IOException {
         List<byte[]> tuple = new ArrayList<>();
         // cycle for || by function
+        tuple.add(new byte[4]);
         tuple.add(new byte[60]);
         tuple.add(new byte[60]);
         tuple.add(new byte[4]);
         for(int i=0;raf.getFilePointer()!=raf.length(); i = (i +1) % tuple.size()){
             raf.readFully(tuple.get(i));
             if(i==tuple.size()-1){
-                byte[] byteArray = (byte[]) tuple.get(0); // Supponendo che tuple.get(0) sia un array di byte
-                System.out.println(Arrays.toString(byteArray));
-                System.out.println(tuple.get(i).toString().replaceAll("\\s", ""));
-
-                System.out.println(new String(tuple.get(1)));
+                System.out.print(new String(tuple.get(0))+" ");
+                System.out.print(new String(tuple.get(1))+" ");
                 System.out.println(ByteBuffer.wrap(tuple.get(2)).getInt());
             }
         }
     }
-    public void parse(){
-
-    }
     public static void main(String[] args) {
-        Gestion gestion = new Gestion();
+        Gestion gestion = new Gestion("../example.csv");
         gestion.gestionFile(1);
         gestion.gestionFile(2);
     }
